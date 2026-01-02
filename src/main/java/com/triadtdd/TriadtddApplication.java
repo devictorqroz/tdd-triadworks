736ad49f4028b8f4e6a29bb43a1046bf9b9f8379
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-01-02 09:40:05 -0300

    Initial commit

---
+++ src/main/java/com/triadtdd/TriadtddApplication.java
@@ -0,0 +1,13 @@
+package com.triadtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriadtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriadtddApplication.class, args);
+	}
+
+}
